7d7c35c8245592c9642b075f986b09e08e29f7a3
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-19 08:39:37 +0200

    Updates

--- source/apps/contacts/views/package copy.d
+++
@@ -1,7 +0,0 @@
-module apps.contacts.views;
-
-public import apps.contacts.views.components;
-public import apps.contacts.views.entities;
-
-public import apps.contacts.views.error;
-public import apps.contacts.views.index;
